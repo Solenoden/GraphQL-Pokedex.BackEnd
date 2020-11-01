0ae1405daef162ac07fa1cd5c0eaadd01d65bde5
Gavin de Bruyn <gavin@mitnet.co.za>
2020-11-01 13:54:19 +0200

    Created mutators to create a new pokemon and update an existing pokemon
    Signed-off-by: Gavin de Bruyn <gavindb@runninghill.co.za>

---
+++ models/Pokemon.js
@@ -0,0 +1,9 @@
+class Pokemon {
+    constructor(id, name, type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+}
+
+module.exports = Pokemon;
--- resolvers/pokemonResolver.js
+++ resolvers/pokemonResolver.js
@@ -1,10 +1,14 @@
-const {GraphQLList, GraphQLInt} = require('graphql');
+const {GraphQLList, GraphQLInt, GraphQLString} = require('graphql');
 const {pokemon} = require('../data/pokemonData');
 const {PokemonType} = require('../types/PokemonType');
+const Pokemon = require('../models/Pokemon');
 
-exports.register = (rootQueryConfig) => {
+exports.register = (rootQueryConfig, rootMutationConfig) => {
     rootQueryConfig.fields.pokemonGetAll = pokemonGetAll;
     rootQueryConfig.fields.pokemonGetById = pokemonGetById;
+
+    rootMutationConfig.fields.pokemonCreate = pokemonCreate;
+    rootMutationConfig.fields.pokemonUpdate = pokemonUpdate;
 };
 
 const pokemonGetAll = {
@@ -17,9 +21,40 @@ const pokemonGetAll = {
 const pokemonGetById = {
     type: PokemonType,
     args: {
-      id: {type: GraphQLInt}
+        id: {type: GraphQLInt}
     },
     resolve(parent, args) {
         return pokemon.find(currentPokemon => currentPokemon.id === args.id);
+    }
+}
+
+const pokemonCreate = {
+    type: PokemonType,
+    args: {
+        id: {type: GraphQLInt},
+        name: {type: GraphQLString},
+        type: {type: GraphQLString}
+    },
+    resolve(parent, args) {
+        let newPokemon = new Pokemon(args.id, args.name, args.type);
+        pokemon.push(newPokemon);
+
+        return newPokemon;
+    }
+}
+
+const pokemonUpdate = {
+    type: PokemonType,
+    args: {
+        id: {type: GraphQLInt},
+        name: {type: GraphQLString},
+        type: {type: GraphQLString}
+    },
+    resolve(parent, args) {
+        let pokemonToUpdate = pokemon.find(currentPokemon => currentPokemon.id === args.id);
+        pokemonToUpdate.name = args.name;
+        pokemonToUpdate.type = args.type;
+
+        return pokemonToUpdate;
     }
 }
